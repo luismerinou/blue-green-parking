c9d810af500838e7dc443b744bc17a141c77d5a8
Luis Merino Ulizarna <luis.merino@nextdigital.es>
2025-03-23 19:52:07 +0100

    :sparkles: feat: add user location with folium map

---
+++ user-location.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import logging
+import sys
+import folium
+from streamlit_folium import st_folium
+from streamlit_geolocation import streamlit_geolocation
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+MADRID_SOL = {'lat': 40.416609, 'lon': -3.702556}
+
+
+def create_map(latitude, longitude, zoom_start=15):
+    mapa = folium.Map(location=[latitude, longitude], zoom_start=zoom_start)
+    folium.Marker([latitude, longitude], popup="¡Aquí estás!").add_to(mapa)
+    return mapa
+
+
+def get_location():
+    location = streamlit_geolocation()
+    latitude = location.get("latitude", "N/A")
+    longitude = location.get("longitude", "N/A")
+    accuracy = location.get("accuracy", "N/A")
+
+    if latitude is None or longitude is None:
+        latitude, longitude = st.session_state.get("latitude"), st.session_state.get("longitude") # Puerta del Sol, Madrid
+
+    return latitude, longitude, accuracy
+
+
+def render_map(mapa):
+    map_key = f"mapa_{st.session_state['latitude']}_{st.session_state['longitude']}"
+    st_folium(mapa, width=700, height=500, key=map_key)
+
+
+def init_session_state():
+    if "map_initialized" not in st.session_state:
+        st.session_state["map_initialized"] = False
+        st.session_state["latitude"] = MADRID_SOL.get("lat")
+        st.session_state["longitude"] = MADRID_SOL.get("lon")
+
+
+def main():
+    init_session_state()
+
+    lat, long, accuracy = get_location()
+
+    logger.info(f"Ubicación obtenida: Latitud: {lat}, Longitud: {long}")
+
+    st.write(f"Ubicación obtenida: Latitud: {lat}, Longitud: {long}, Accuracy: {accuracy}")
+
+    mapa = create_map(lat, long)
+    render_map(mapa)
+
+
+if __name__ == '__main__':
+    main()
