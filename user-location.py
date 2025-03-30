f83703b29657bb6762b4099b04f5df4990ea5494
Luis Merino Ulizarna <luis.merino@nextdigital.es>
2025-03-30 12:46:04 +0200

    :recycle: refactor: update exception handling on LocationError

--- user-location.py
+++ user-location.py
@@ -5,6 +5,8 @@ import folium
 from streamlit_folium import st_folium
 from streamlit_current_location import current_position
 
+from custom_exceptions.LocationError import LocationError
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(
@@ -15,28 +17,11 @@ logging.basicConfig(
 MADRID_SOL = {'lat': 40.416609, 'lon': -3.702556}
 
 
-def create_map(latitude, longitude, zoom_start=15):
+def create_map(latitude, longitude, zoom_start=15, add_marker=False):
     mapa = folium.Map(location=[latitude, longitude], zoom_start=zoom_start)
-    folium.Marker([latitude, longitude], popup="¡Aquí estás!").add_to(mapa)
+    if add_marker:
+        folium.Marker([latitude, longitude], popup="¡Aquí estás!").add_to(mapa)
     return mapa
-
-
-def get_location():
-    location = current_position()
-    st.write(
-        f"""your location is 
-        latitude: {location['latitude']}
-        longitude: {location['longitude']}
-        """
-    )
-    latitude = location["latitude"]
-    longitude = location["longitude"]
-    accuracy = 100
-
-    if latitude is None or longitude is None:
-        latitude, longitude = st.session_state.get("latitude"), st.session_state.get("longitude") # Puerta del Sol, Madrid
-
-    return latitude, longitude, accuracy
 
 
 def render_map(mapa):
@@ -51,17 +36,43 @@ def init_session_state():
         st.session_state["longitude"] = MADRID_SOL.get("lon")
 
 
+def get_location():
+    location = current_position()
+    if not location:
+        return MADRID_SOL.get('lat'), MADRID_SOL.get('lon'), 100
+    else:
+        st.write(
+            f"""Tu ubicación es 
+            Latitud: {location.get('latitude', 'N/A')}
+            Longitud: {location.get('longitude', 'N/A')}
+            """
+        )
+        latitude = location.get('latitude', 'N/A')
+        longitude = location.get('longitude', 'N/A')
+        accuracy = 100
+
+        if None in (latitude, longitude, accuracy):
+            raise LocationError(
+                "Faltan datos de ubicación.",
+                location_data=location
+            )
+        else:
+            logger.info(f"Ubicación obtenida: Latitud: {latitude}, Longitud: {longitude}, Accuracy: {accuracy}")
+            return latitude, longitude, accuracy
+
+
 def main():
     init_session_state()
+    try:
+        lat, long, accuracy = get_location()
 
-    lat, long, accuracy = get_location()
+        st.write(f"Ubicación obtenida: Latitud: {lat}, Longitud: {long}, Accuracy: {accuracy}")
 
-    logger.info(f"Ubicación obtenida: Latitud: {lat}, Longitud: {long}")
+        mapa = create_map(lat, long, add_marker=True)
+        render_map(mapa)
 
-    st.write(f"Ubicación obtenida: Latitud: {lat}, Longitud: {long}, Accuracy: {accuracy}")
-
-    mapa = create_map(lat, long)
-    render_map(mapa)
+    except Exception as e:
+        logger.error(e)
 
 
 if __name__ == '__main__':
